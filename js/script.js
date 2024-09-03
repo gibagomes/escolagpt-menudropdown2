4ee811e4d08862f7d1c1c3858ff20f59a968f840
Escola GPT <66352542+gibagomes@users.noreply.github.com>
2024-09-03 04:53:09 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,22 @@
+// Optional: Add a click event listener to the dropdown button
+document.querySelector(".dropbtn").addEventListener("click", function () {
+  var dropdownContent = this.nextElementSibling;
+  if (dropdownContent.style.display === "block") {
+    dropdownContent.style.display = "none";
+  } else {
+    dropdownContent.style.display = "block";
+  }
+});
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function (event) {
+  if (!event.target.matches(".dropbtn")) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    for (var i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.style.display === "block") {
+        openDropdown.style.display = "none";
+      }
+    }
+  }
+};
